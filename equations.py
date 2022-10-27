63120d18093bc18f4b7dfd377f61eee9175b5fb7
xx987 <70827331+xx987@users.noreply.github.com>
2022-10-27 16:18:02 -0500

    Add files via upload

---
+++ equations.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from timesteppers import StateVector
+from scipy import sparse
+import numpy as np
+
+from finite import DifferenceNonUniformGrid, DifferenceUniformGrid
+
+
+
+class ViscousBurgers:
+    
+    def __init__(self, u, nu, d, d2):
+        self.u = u
+        self.X = StateVector([u])
+        
+        N = len(u)
+        self.M = sparse.eye(N, N)
+        self.L = -nu*d2.matrix
+        
+        f = lambda X: -X.data*(d @ X.data)
+        
+        self.F = f
+
+
+class Wave:
+    
+    def __init__(self, u, v, d2):
+        self.X = StateVector([u, v])
+        N = len(u)
+        I = sparse.eye(N, N)
+        Z = sparse.csr_matrix((N, N))
+
+        M00 = I
+        M01 = Z
+        M10 = Z
+        M11 = I
+        self.M = sparse.bmat([[M00, M01],
+                              [M10, M11]])
+
+        L00 = Z
+        L01 = -I
+        L10 = -d2.matrix
+        L11 = Z
+        self.L = sparse.bmat([[L00, L01],
+                              [L10, L11]])
+
+        
+        self.F = lambda X: 0*X.data
+
+
+class SoundWave:
+    
+    def __init__(self, u, p, d, rho0, gammap0):
+        leng = len(c)
+        Z = sparse.csr_matrix((leng, leng))
+        I = sparse.eye(leng, leng)  
+        self.X = StateVector([u, p])
+        self.M = sparse.bmat([[rho0 * I, Z], [Z, I]])
+        self.L = sparse.bmat([[Z, d.matrix], [gammap0 * d.matrix, Z]])
+        self.F = lambda X: np.zeros(X.data.shape)
+
+
+class ReactionDiffusion:
+    
+    def __init__(self, c, d2, c_target, D):
+        
+        
+        leng = len(c)
+        self.L = -D * d2.matrix
+        self.X = StateVector([c])
+        self.M = sparse.eye(leng, leng)
+        self.F = lambda X: X.data * (c_target - X.data)
+
