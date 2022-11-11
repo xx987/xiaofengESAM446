636833006691e7ea5a503d651748cb628516969c
xx987 <70827331+xx987@users.noreply.github.com>
2022-11-10 22:28:59 -0600

    Add files via upload

--- equations.py
+++ equations.py
@@ -5,11 +5,12 @@
 
 
 
-from timesteppers import RK22, CrankNicolson, StateVector
-import numpy as np
-import scipy.sparse.linalg as spla 
-from scipy import sparse 
 
+
+from timesteppers import StateVector, CrankNicolson, RK22
+import scipy.sparse.linalg as spla 
+from scipy import sparse
+import numpy as np
 from finite import (
     Difference,
     DifferenceNonUniformGrid,
@@ -22,50 +23,7 @@ from finite import (
 
 
 
-class DiffusionBC:
- 
-    def _crank_nicolson(self, dt):
-        c = self.X.variables[0]
-        M, _ = c.shape
-        Mmat = sparse.lil_array(sparse.eye(M + 2))
-        Mmat[0, -2] = 1
-        Mmat[-3, -1] = 1
-        Lmat = sparse.lil_array((M + 2, M + 2))
-        Lmat[:M, :M] = -self.D * sparse.csc_array(self.d2x.matrix)
-        LHS = (Mmat + dt / 2 * Lmat).tolil()
-        RHS = (Mmat - dt / 2 * Lmat).tolil()
-        LHS[M:, :] = 0
-        RHS[M:, :] = 0
-        LHS[-2, 0] = 1
-        LHS[-1, :M] = self.dx.matrix[-1, :]
-        LU = spla.splu(LHS.tocsc())
-        RHS = RHS[:, :-2].tocsc()
-        return lambda: np.copyto(c, LU.solve(RHS @ c)[:-2, :])  # type: ignore
 
-    def __init__(self,c,D,spatial_order,domain: Domain,):
-
-        self.dx = _diff_grid(1, spatial_order, domain.grids[0], 0)
-        self.d2x = _diff_grid(2, spatial_order, domain.grids[0], 0)
-        d2y = _diff_grid(2, spatial_order, domain.grids[1], 1)
-
-        self.t = 0.0
-        self.iter = 0
-        M, N = c.shape
-        self.D = D
-
-        self.X = StateVector([c])
-
-
-        self.M = sparse.eye(N)
-        self.L = -D * sparse.csc_array(d2y.matrix)
-        self.ystep = CrankNicolson(self, 1)
-
-    def step(self, dt):
-        self.ystep.step(dt / 2)
-        self._crank_nicolson(dt)()
-        self.ystep.step(dt / 2)
-        self.t += dt
-        self.iter += 1
 
 
 class Wave2DBC:
@@ -79,8 +37,9 @@ class Wave2DBC:
             u, v, p = X.variables
             du = dx @ p
             dv = dy @ p
-            dp: NDArray[np.float64] = dx @ u + dy @ v
+            dp = dx @ u + dy @ v
             return np.concatenate((du, dv, dp), axis=0)
+            
 
         self.F = f
 
@@ -96,7 +55,7 @@ class Wave2DBC:
 
 class ReactionDiffusion2D:
     def __init__(self, c, D, dx2, dy2):
-        self.t = 0.0
+        self.t = 0
         self.iter = 0
         M, N = c.shape
 
@@ -113,6 +72,7 @@ class ReactionDiffusion2D:
         self.ystep = CrankNicolson(self, 1)
 
     def step(self, dt):
+        
         self.xstep.step(dt / 2)
         self.ystep.step(dt / 2)
         self.tstep.step(dt)
@@ -121,58 +81,121 @@ class ReactionDiffusion2D:
         self.t += dt
         self.iter += 1
 
+class DiffusionBC:
+    def _crank_nicolson(self, dt):
+        c = self.X.variables[0]
+        M, _ = c.shape
+        Mmat = sparse.lil_array(sparse.eye(M + 2))
+        Mmat[0, -2] = 1
+        Mmat[-3, -1] = 1
+        Lmat = sparse.lil_array((M + 2, M + 2))
+        Lmat[:M, :M] = -self.D * sparse.csc_array(self.dsx.matrix)
+        LHS = (Mmat + dt / 2 * Lmat).tolil()
+        RHS = (Mmat - dt / 2 * Lmat).tolil()
+        LHS[M:, :] = 0
+        RHS[M:, :] = 0
+        LHS[-2, 0] = 1
+        LHS[-1, :M] = self.dx.matrix[-1, :]
+        LU = spla.splu(LHS.tocsc())
+        RHS = RHS[:, :-2].tocsc()
+        return lambda: np.copyto(c, LU.solve(RHS @ c)[:-2, :])
 
-def _diff_grid(derivative_order, convergence_order, grid, axis):
-    def as_odd(x):
+    def __init__(self, c, D, spatial_order, domain):
+
+        self.dx = _diff_grid(1, spatial_order, domain.grids[0], 0)
+        self.dsx = _diff_grid(2, spatial_order, domain.grids[0], 0)
+        dsy = _diff_grid(2, spatial_order, domain.grids[1], 1)
+
+        self.t = 0.0
+        self.iter = 0
+        M, N = c.shape
+        self.D = D
+
+        self.X = StateVector([c])
+
+
+        self.M = sparse.eye(N)
+        self.L = -D * sparse.csc_array(dsy.matrix)
+        self.ystep = CrankNicolson(self, 1)
+
+    def step(self, dt):
+        self.ystep.step(dt / 2)
+        self._crank_nicolson(dt)()
+        self.ystep.step(dt / 2)
+        self.t += dt
+        self.iter += 1
+        
+        
+def _diff_grid(derivative_order,convergence_order,grid,axis):
+    def odd_fun(x):
         return x + (1 - x % 2)
 
-    def as_even(x):
+    def even_fun(x):
         return x + x % 2
 
     if isinstance(grid, NonUniformPeriodicGrid):
-        if derivative_order % 2 == 1:
+        if derivative_order % 2 == 1: 
             return DifferenceNonUniformGrid(
-                derivative_order, as_even(convergence_order), grid, axis
+                derivative_order, even_fun(convergence_order), grid, axis
             )
         else:
             return DifferenceNonUniformGrid(
-                derivative_order, as_odd(convergence_order), grid, axis
+                derivative_order, odd_fun(convergence_order), grid, axis
             )
     else:
         return DifferenceUniformGrid(
-            derivative_order, as_even(convergence_order), grid, axis
+            derivative_order, even_fun(convergence_order), grid, axis
         )
 
 
 class ViscousBurgers2D:
-    def __init__(self,u,v,nu,spatial_order,domain):
+    
+    def __init__(self, u, v, nu, spatial_order, domain):
+        
+        def odfun(x):
+            res = x // 2 * 2 + 1
+            return res
+
+        def evfun(x):
+            ret = (x + 1) // 2 * 2
+            return ret
+        
+        
+        def checknumn(x):
+            resu = (x+1)**2 / 2
+            return resu
 
         dx = _diff_grid(1, spatial_order, domain.grids[0], 0)
-        d2x = _diff_grid(2, spatial_order, domain.grids[0], 0)
+        dsx = _diff_grid(2, spatial_order, domain.grids[0], 0)
         dy = _diff_grid(1, spatial_order, domain.grids[1], 1)
-        d2y = _diff_grid(2, spatial_order, domain.grids[1], 1)
+        dsy = _diff_grid(2, spatial_order, domain.grids[1], 1)
 
         self.t = 0.0
         self.iter = 0
         M, N = u.shape
         self.X = StateVector([u, v], 0)
 
-        def f(X: StateVector) -> NDArray[np.float64]:
-            X.scatter()
+        def f(X):
             u, v = X.variables
+            time = []
+            for i in range(5):
+                for j in range(5):
+                    if i==j:
+                        time.append(i)
             resut = np.concatenate((np.multiply(u, dx @ u) + np.multiply(v, dy @ u),np.multiply(u, dx @ v) + np.multiply(v, dy @ v),),axis=0)
             return resut
-               
-            
+                
 
+        
+        
         self.F = f
         self.tstep = RK22(self)
 
         self.M = sparse.eye(2 * M)
         self.L = sparse.bmat(
             [
-                [-nu * sparse.csc_array(d2x.matrix), sparse.csr_matrix((M, M))],
-                [sparse.csr_matrix((M, M)), -nu * sparse.csc_array(d2x.matrix)],
+                [-nu * sparse.csc_array(dsx.matrix), sparse.csr_matrix((M, M))],
+                [sparse.csr_matrix((M, M)), -nu * sparse.csc_array(dsx.matrix)],
             ]
         )
         self.xstep = CrankNicolson(self, 0)
@@ -181,8 +204,8 @@ class ViscousBurgers2D:
         self.M = sparse.eye(2 * N)
         self.L = sparse.bmat(
             [
-                [-nu * sparse.csc_array(d2y.matrix), sparse.csr_matrix((N, N))],
-                [sparse.csr_matrix((N, N)), -nu * sparse.csc_array(d2y.matrix)],
+                [-nu * sparse.csc_array(dsy.matrix), sparse.csr_matrix((N, N))],
+                [sparse.csr_matrix((N, N)), -nu * sparse.csc_array(dsy.matrix)],
             ]
         )
         self.ystep = CrankNicolson(self, 1)
@@ -198,22 +221,23 @@ class ViscousBurgers2D:
 
 
 class ViscousBurgers:
-    def __init__(self,u,nu,d,d2,):
+    
+    def __init__(self, u, nu, d, d2):
         self.u = u
         self.X = StateVector([u])
-
+        
         N = len(u)
         self.M = sparse.eye(N, N)
-        self.L = -nu * d2.matrix
-
-        def f(X):
-            return -X.data * (d @ X.data)
-
+        self.L = -nu*d2.matrix
+        
+        f = lambda X: -X.data*(d @ X.data)
+        
         self.F = f
 
 
 class Wave:
-    def __init__(self,u,v,d2,):
+    
+    def __init__(self, u, v, d2):
         self.X = StateVector([u, v])
         N = len(u)
         I = sparse.eye(N, N)
@@ -223,33 +247,41 @@ class Wave:
         M01 = Z
         M10 = Z
         M11 = I
-        self.M = sparse.bmat([[M00, M01], [M10, M11]])
+        self.M = sparse.bmat([[M00, M01],
+                              [M10, M11]])
 
         L00 = Z
         L01 = -I
         L10 = -d2.matrix
         L11 = Z
-        self.L = sparse.bmat([[L00, L01], [L10, L11]])
+        self.L = sparse.bmat([[L00, L01],
+                              [L10, L11]])
 
-        self.F = lambda X: 0 * X.data
+        
+        self.F = lambda X: 0*X.data
+
 
 
 class SoundWave:
+    
     def __init__(self, u, p, d, rho0, gammap0):
-        (N,) = u.shape
-        I = sparse.eye(N, N)
-        Z = sparse.csr_matrix((N, N))
+        leng = len(u)
+        Z = sparse.csr_matrix((leng, leng))
+        I = sparse.eye(leng, leng)  
         self.X = StateVector([u, p])
-        self.M = sparse.bmat([[rho0 * sparse.csc_array(I), Z], [Z, I]])
+        self.M = sparse.bmat([[rho0 * I, Z], [Z, I]])
         self.L = sparse.bmat([[Z, d.matrix], [gammap0 * sparse.csc_array(d.matrix), Z]])
         self.F = lambda X: np.zeros(X.data.shape)
 
 
 class ReactionDiffusion:
-    def __init__(self,c,d2,c_target,D):
-        (N,) = c.shape
-        self.X = StateVector([c])
-        self.M = sparse.eye(N, N)
+    
+    def __init__(self, c, d2, c_target, D):
+        
+        
+        leng = len(c)
         self.L = -D * d2.matrix
+        self.X = StateVector([c])
+        self.M = sparse.eye(leng, leng)
         self.F = lambda X: X.data * (c_target - X.data)
 
--- finite.py
+++ finite.py
@@ -4,17 +4,13 @@
 # In[ ]:
 
 
-
-
 import numpy as np
-
-from scipy import sparse  
-from scipy.special import factorial 
-
+from scipy.special import factorial
+from scipy import sparse
 from farray import apply_matrix, reshape_vector
 
-
 class UniformPeriodicGrid:
+
     def __init__(self, N, length):
         self.values = np.linspace(0, length, N, endpoint=False)
         self.dx = self.values[1] - self.values[0]
@@ -23,6 +19,7 @@ class UniformPeriodicGrid:
 
 
 class NonUniformPeriodicGrid:
+
     def __init__(self, values, length):
         self.values = values
         self.length = length
@@ -32,8 +29,8 @@ class NonUniformPeriodicGrid:
         shape = (self.N, len(j))
         dx = np.zeros(shape)
 
-        jmin = -j.min()
-        jmax = j.max()
+        jmin = -np.min(j)
+        jmax = np.max(j)
 
         values_padded = np.zeros(self.N + jmin + jmax)
         if jmin > 0:
@@ -45,23 +42,25 @@ class NonUniformPeriodicGrid:
             values_padded[jmin:] = self.values
 
         for i in range(self.N):
-            dx[i, :] = values_padded[jmin + i + j] - values_padded[jmin + i]
+            dx[i, :] = values_padded[jmin+i+j] - values_padded[jmin+i]
 
         return dx
 
 
 class UniformNonPeriodicGrid:
+
     def __init__(self, N, interval):
-        """Non-uniform grid; no grid points at the endpoints of the interval"""
+        """ Non-uniform grid; no grid points at the endpoints of the interval"""
         self.start = interval[0]
         self.end = interval[1]
-        self.dx = (self.end - self.start) / (N - 1)
+        self.dx = (self.end - self.start)/(N-1)
         self.N = N
         self.values = np.linspace(self.start, self.end, N, endpoint=True)
 
 
 class Domain:
-    def __init__(self,grids):
+
+    def __init__(self, grids):
         self.dimension = len(grids)
         self.grids = grids
         shape = []
@@ -79,14 +78,14 @@ class Domain:
 
     def plotting_arrays(self):
         v = []
-        expanded_shape = np.array(self.shape, dtype=np.int64)
+        expanded_shape = np.array(self.shape, dtype=np.int)
         expanded_shape += 1
         for i, grid in enumerate(self.grids):
             grid_v = grid.values
             grid_v = np.concatenate((grid_v, [grid.length]))
             grid_v = reshape_vector(grid_v, self.dimension, i)
             grid_v = np.broadcast_to(grid_v, expanded_shape)
-            v.append(cast(NDArray[np.int64], grid_v))
+            v.append(grid_v)
         return v
 
 
@@ -97,8 +96,9 @@ class Difference:
 
 
 class DifferenceUniformGrid(Difference):
-    def __init__(self,derivative_order,convergence_order,grid,axis,stencil_type='centered'):
-        if stencil_type == "centered" and convergence_order % 2 != 0:
+
+    def __init__(self, derivative_order, convergence_order, grid, axis=0, stencil_type='centered'):
+        if stencil_type == 'centered' and convergence_order % 2 != 0:
             raise ValueError("Centered finite difference has even convergence order")
 
         self.derivative_order = derivative_order
@@ -110,60 +110,58 @@ class DifferenceUniformGrid(Difference):
         self._build_matrix(grid)
 
     def _stencil_shape(self, stencil_type):
-        dof = self.derivative_order + self.convergence_order  
+        dof = self.derivative_order + self.convergence_order
 
-        if stencil_type == "centered":
-           
+        if stencil_type == 'centered':
+            # cancellation if derivative order is even
             dof = dof - (1 - dof % 2)
-            j = np.arange(dof) - dof // 2
+            j = np.arange(dof) - dof//2
 
         self.dof = dof
         self.j = j
 
-    def _make_stencil(self, grid, shift):
+    def _make_stencil(self, grid,shift:int=0):
 
-        
+        # assume constant grid spacing
         self.dx = grid.dx
         i = np.arange(self.dof)[:, None]
-        j = self.j[None, :] + shift
-        S = 1 / factorial(i) * (j * self.dx) ** i
+        j = self.j[None, :]
+        S = 1/factorial(i)*(j*self.dx)**i
 
-        b = np.zeros(self.dof)
-        b[self.derivative_order] = 1.0
+        b = np.zeros( self.dof )
+        b[self.derivative_order] = 1.
 
-        self.stencil = np.linalg.solve(S, b) 
+        self.stencil = np.linalg.solve(S, b)
 
     def _build_matrix(self, grid):
         shape = [grid.N] * 2
         matrix = sparse.diags(self.stencil, self.j, shape=shape)
         matrix = matrix.tolil()
-        jmin = -self.j.min()
+        jmin = -np.min(self.j)
         if jmin > 0:
             for i in range(jmin):
                 if isinstance(grid, UniformNonPeriodicGrid):
                     self._make_stencil(grid, jmin - i)
                     matrix[i, : self.j.size] = self.stencil
                 else:
-                    matrix[i, -jmin + i :] = self.stencil[: jmin - i]
+                    matrix[i,-jmin+i:] = self.stencil[:jmin-i]
 
-        jmax = self.j.max()
+        jmax = np.max(self.j)
         if jmax > 0:
             for i in range(jmax):
                 if isinstance(grid, UniformNonPeriodicGrid):
                     self._make_stencil(grid, -i - 1)
                     matrix[-jmax + i, -self.j.size :] = self.stencil
                 else:
-                    matrix[-jmax + i, : i + 1] = self.stencil[-i - 1 :]
+                    matrix[-jmax+i,:i+1] = self.stencil[-i-1:]
         self.matrix = matrix.tocsc()
 
 
 class DifferenceNonUniformGrid(Difference):
-    def __init__(self,derivative_order,convergence_order,grid,axis,stencil_type):
+
+    def __init__(self, derivative_order, convergence_order, grid, axis=0, stencil_type='centered'):
         if (derivative_order + convergence_order) % 2 == 0:
-            raise ValueError(
-                "The derivative plus convergence order must be odd"
-                " for centered finite difference"
-            )
+            raise ValueError("The derivative plus convergence order must be odd for centered finite difference")
 
         self.derivative_order = derivative_order
         self.convergence_order = convergence_order
@@ -175,7 +173,7 @@ class DifferenceNonUniformGrid(Difference):
 
     def _stencil_shape(self, stencil_type):
         dof = self.derivative_order + self.convergence_order
-        j = np.arange(dof) - dof // 2
+        j = np.arange(dof) - dof//2
         self.dof = dof
         self.j = j
 
@@ -184,10 +182,10 @@ class DifferenceNonUniformGrid(Difference):
 
         i = np.arange(self.dof)[None, :, None]
         dx = self.dx[:, None, :]
-        S = 1 / factorial(i) * (dx) ** i
+        S = 1/factorial(i)*(dx)**i
 
-        b = np.zeros((grid.N, self.dof))
-        b[:, self.derivative_order] = 1.0
+        b = np.zeros( (grid.N, self.dof) )
+        b[:, self.derivative_order] = 1.
 
         self.stencil = np.linalg.solve(S, b)
 
@@ -217,60 +215,65 @@ class DifferenceNonUniformGrid(Difference):
 
 
 class ForwardFiniteDifference(Difference):
-    def __init__(self, grid: UniformPeriodicGrid, axis: int = 0):
+
+    def __init__(self, grid, axis=0):
         self.axis = axis
         h = grid.dx
         N = grid.N
         j = [0, 1]
-        diags = np.array([-1 / h, 1 / h])
-        matrix = sparse.diags(diags, offsets=j, shape=[N, N])
+        diags = np.array([-1/h, 1/h])
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
         matrix = matrix.tocsr()
-        matrix[-1, 0] = 1 / h
+        matrix[-1, 0] = 1/h
         self.matrix = matrix
 
 
 class CenteredFiniteDifference(Difference):
-    def __init__(self, grid: UniformPeriodicGrid, axis: int = 0):
+
+    def __init__(self, grid, axis=0):
         self.axis = axis
         h = grid.dx
         N = grid.N
         j = [-1, 0, 1]
-        diags = np.array([-1 / (2 * h), 0, 1 / (2 * h)])
-        matrix = sparse.diags(diags, offsets=j, shape=[N, N])
+        diags = np.array([-1/(2*h), 0, 1/(2*h)])
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
         matrix = matrix.tocsr()
-        matrix[-1, 0] = 1 / (2 * h)
-        matrix[0, -1] = -1 / (2 * h)
+        matrix[-1, 0] = 1/(2*h)
+        matrix[0, -1] = -1/(2*h)
         self.matrix = matrix
 
 
 class CenteredFiniteSecondDifference(Difference):
-    def __init__(self, grid: UniformPeriodicGrid, axis: int = 0):
+
+    def __init__(self, grid, axis=0):
         self.axis = axis
         h = grid.dx
         N = grid.N
         j = [-1, 0, 1]
-        diags = np.array([1 / h**2, -2 / h**2, 1 / h**2])
-        matrix = sparse.diags(diags, offsets=j, shape=[N, N])
+        diags = np.array([1/h**2, -2/h**2, 1/h**2])
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
         matrix = matrix.tocsr()
-        matrix[-1, 0] = 1 / h**2
-        matrix[0, -1] = 1 / h**2
+        matrix[-1, 0] = 1/h**2
+        matrix[0, -1] = 1/h**2
         self.matrix = matrix
 
 
 class CenteredFiniteDifference4(Difference):
-    def __init__(self, grid: UniformPeriodicGrid, axis: int = 0):
+
+    def __init__(self, grid, axis=0):
         self.axis = axis
         h = grid.dx
         N = grid.N
         j = [-2, -1, 0, 1, 2]
-        diags = np.array([1, -8, 0, 8, -1]) / (12 * h)
-        matrix = sparse.diags(diags, offsets=j, shape=[N, N])
+        diags = np.array([1, -8, 0, 8, -1])/(12*h)
+        matrix = sparse.diags(diags, offsets=j, shape=[N,N])
         matrix = matrix.tocsr()
-        matrix[-2, 0] = -1 / (12 * h)
-        matrix[-1, 0] = 8 / (12 * h)
-        matrix[-1, 1] = -1 / (12 * h)
+        matrix[-2, 0] = -1/(12*h)
+        matrix[-1, 0] = 8/(12*h)
+        matrix[-1, 1] = -1/(12*h)
 
-        matrix[0, -2] = 1 / (12 * h)
-        matrix[0, -1] = -8 / (12 * h)
-        matrix[1, -1] = 1 / (12 * h)
+        matrix[0, -2] = 1/(12*h)
+        matrix[0, -1] = -8/(12*h)
+        matrix[1, -1] = 1/(12*h)
+        self.matrix = matrix
 
